4294fa344b90a32b65dff9027c7eb1cb21a673b8
hesanghasemi <hesanghasemi99@gmail.com>
2020-02-11 21:22:42 +0330

    finishing sign in handleSubmit method

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -20,7 +20,7 @@ class SignUp extends Component {
 
     const { displayName, email, password, confirmPassword } = this.state;
 
-    if (password != confirmPassword) {
+    if (password !== confirmPassword) {
       alert("passwords don't match");
       return;
     }
